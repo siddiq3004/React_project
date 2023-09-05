fc4cf22b4606f335bd28d9663f5322164b0f3fb4
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2023-09-05 18:15:01 +0530

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import {
+  ClerkProvider, SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
+
+if (!process.env.REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
 function App() {
   return (
     <>
-      <div className="">
-        hello dostoon
-      </div>
+      <ClerkProvider publishableKey={clerkPubKey}>
+        <SignedIn>
+          <div className="text-3xl text-red-800">
+            hello dostoon(protected)
+            <UserButton/>
+          </div>
+        </SignedIn>
+        <SignedOut>
+          <RedirectToSignIn />
+        </SignedOut>
+      </ClerkProvider>
     </>
   );
 }
